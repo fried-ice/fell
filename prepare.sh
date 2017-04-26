6b59edc476d50ebda788d459d351a746f3d93591
Fried.Ice <fried.ice.94@gmail.com>
2017-04-26 18:55:22 +0200

    enable pandoc in travis

--- prepare.sh
+++ prepare.sh
@@ -11,5 +11,7 @@ automake --add-missing
 # 	First: Search for all ".md" files and execute pandoc on them, creating a man page with the extension .md.tmp
 # 	Second: Rename all those files removing the.md.tmp file endings (the last 7 characters)
 
+command -v pandoc >/dev/null 2>&1 || { echo >&2 "Pandoc is required for building man pages. Aborting."; exit 1; }
+
 find ./man -type f -name "*.md" -exec pandoc "{}" -s -t man -o "{}.tmp" \;
 find ./man -type f -regextype sed -regex ".*/.*\.[0-9].md.tmp$" | while read f; do mv "$f" "${f%???????}"; done
